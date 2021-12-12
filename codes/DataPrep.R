15b95bbeb7072472a36ad2cea4b2a73c1c6154a7
Enrico Sartor <40143941+dorianpastor@users.noreply.github.com>
2021-12-12 17:09:33 +0100

    updated

--- codes/DataPrep.R
+++ codes/DataPrep.R
@@ -484,7 +484,8 @@ DATA$renovate_index =  c(ifelse(yr_renovated==0,DATA$yr_old,2015-yr_renovated))
 DATA$has_ren = as.integer(as.logical(yr_renovated!=0))  # If it was renovated
 DATA$has_bas = as.integer(as.logical(sqft_basement!=0))  # If it has basement, since many don't have it
 DATA$ord_date = ordinal_date  # To take into account the market evolution
-DATA <- cbind(DATA,log10(price),bathfloors_ratio,log10(sqm_living),log10(sqm_lot),log10(sqm_living15),log10(sqm_lot15),geodist_index)
+
+DATA <- cbind(DATA,log10(price), bedfloors_ratio, bathfloors_ratio,log10(sqm_living),log10(sqm_lot),log10(sqm_living15),log10(sqm_lot15),geodist_index)
 # Delete
 DATA = subset(DATA, select=-c(sqft_above,sqft_basement,sqft_living,sqft_living15,sqft_lot,sqft_lot15)) # Drop useless columns
 
@@ -493,27 +494,26 @@ DATA = subset(DATA, select=-c(sqft_above,sqft_basement,sqft_living,sqft_living15
 #OUTLIERS DETECTION
 #################################################################
 #Variabili da usare nell'outliers detection: log10(price), bedrooms, bathrooms, 
-# bathfloors_ratio (?), log10(sqm_living), log10(sqm_lot), log10(sqm_living15), geodist_index
+# bathfloors_ratio, bedfloors_ratio, log10(sqm_living), log10(sqm_lot), geodist_index
 # More to try..
 
 # Insert here all the column-couples to put in the bivariate bagplot
 coll_columns = rbind(c("bedrooms","log10(price)"),c("bathrooms","log10(price)"),
-                     c("bathfloors_ratio","log10(price)"),c("log10(sqm_living)","log10(price)"),
-                     c("log10(sqm_lot)","log10(price)"),c("log10(sqm_living15)","log10(price)"),
+                     c("bedfloors_ratio","log10(price)"), c("bathfloors_ratio","log10(price)"),
+                     c("log10(sqm_living)","log10(price)"),c("log10(sqm_lot)","log10(price)"),
                      c("geodist_index","log10(price)"))
 conta = 0  # Here I will count how many outliers we have
 indexes = c()  # Here I will save all the indexes to discard
 for (j in 1:dim(coll_columns)[1]){  # Iterate over all column-couples                
   columns = coll_columns[j,]
   print(columns)
-  x11()
-  bag = bagplot(DATA[columns])
-  out = distinct(data.frame(bag$pxy.outlier))  # distinct to remove duplicates
+  out = distinct(data.frame(bagplot(DATA[columns])$pxy.outlier))  # distinct to remove duplicates
   for (i in 1:dim(out)[1]){  # it can be optimized: loop to search for the outlier in the whole dataset
     row = out[i,]
     get_idx = which(DATA[columns[1]]==as.integer(row["x"]) & DATA[columns[2]]==as.numeric(row["y"]))  # get the indexes where the outlier is found
-    conta = conta + length(get_idx)
+    count_indexes = length(indexes)
     indexes = union(indexes,get_idx)
+    conta = conta + length(indexes) - count_indexes
   }
   print(conta)
 }
